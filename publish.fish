182ea6d1548f56813c96d124a42c6120caf28ca5
Aramís Concepción Durán <aramis@systemli.org>
2023-04-20 11:30:19 +0200

    Ergänze Script publish.fish

---
+++ publish.fish
@@ -0,0 +1,5 @@
+#!/usr/bin/env fish
+git push git@git.eee.gg:aramis/elm-ari
+git push git@git.eee.gg:aramis/elm-ari --tags
+git push git@git.sr.ht:~aramis/elm-ari
+git push git@git.sr.ht:~aramis/elm-ari --tags
