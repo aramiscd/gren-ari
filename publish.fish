ad7e568ac9af2784f37b3ab44ce68d7a4dd093b9
Aramís Concepción Durán <aramis@systemli.org>
2024-06-27 21:05:36 +0200

    publish.fish

--- publish.fish
+++ publish.fish
@@ -4,3 +4,4 @@ git push https://gitea.nojs.de/aramis/ari.git
 git push https://gitea.nojs.de/aramis/ari.git --tags
 git push git@git.sr.ht:~aramis/ari
 git push git@git.sr.ht:~aramis/ari --tags
+git push
